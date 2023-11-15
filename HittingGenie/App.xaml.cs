5de3b69a5343189fef5bd4eb1708571758290f90
rruffing@wynhouse.co <rruffing@wynhouse.co>
2023-11-15 08:58:21 -0500

    initial commit

---
+++ HittingGenie/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HittingGenie;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ HittingGenie/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HittingGenie;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
